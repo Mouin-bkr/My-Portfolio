23aba6c4739435a8ce1fa06f4a11a27be7a59d01
Mouin-bkr <mouinbkr@gmail.com>
2024-02-06 20:50:21 +0000

    last update

--- src/sections/portfolio/data.js
+++ src/sections/portfolio/data.js
@@ -13,8 +13,8 @@ const data = [
         id: 1,
         category: 'frontend',
         image: Image1,
-        title: "Pomodor Focus ",
-        desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+        title: "Pomodoro Focus ",
+        desc: "Pomodoro Focus: Boost productivity with this sleek web app, tailor-made for the Pomodoro technique. Easily set customizable time intervals and colors, empowering users to optimize study or work sessions with efficiency and focus.",
         demo: 'https://pomodoroostudy.netlify.app',
         github: 'https://github.com/Mouin-bkr/Pomodoro_web_app'
     },
@@ -23,7 +23,7 @@ const data = [
         category: 'frontend',
         image: Image2,
         title: "Jungle House",
-        desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+        desc: "Jungle House: An online plant shop, representing one of my initial ventures into web development. Despite its simplicity, its aesthetic appeal remains a personal favorite, showcasing my early journey in crafting visually engaging digital experiences.",
         demo: 'https://planthsopistaken.netlify.app',
         github: 'https://github.com/Mouin-bkr/PlantShop'
     },
@@ -32,7 +32,7 @@ const data = [
         category: 'frontend',
         image: Image3,
         title: "Photo Gallery ",
-        desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+        desc: " A bespoke photo gallery developed for a client, designed to showcase his artwork and personal brand. This project allowed me to merge creativity with functionality, providing a platform for the client to exhibit his talents and achievements.",
         demo: 'https://mouin-bkr.github.io/DraWithMe/',
         github: 'https://github.com/Mouin-bkr/DraWithMe'
     },
@@ -41,7 +41,7 @@ const data = [
         category: 'frontend',
         image: Image4,
         title: "Tape ",
-        desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+        desc: "An elegant recording tool enhanced with animations, tailored for a client's video recording needs. Leveraging CSS and JavaScript, this project seamlessly integrates visual appeal with user functionality, delivering a polished solution for capturing memorable moments.",
         demo: 'https://mouin-bkr.github.io/Tape/',
         github: 'https://github.com/Mouin-bkr/Tape'
     },
@@ -50,7 +50,7 @@ const data = [
         category: 'uiux',
         image: Image5,
         title: "Felnoo ",
-        desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! ",
+        desc: "A comprehensive UI design crafted for a web application aimed at preschools or private schools. This platform streamlines educational activities and preserves student memories, offering a seamless solution for enhancing learning experiences and fostering academic growth.",
         demo: 'https://www.figma.com/file/8RXtLVPJdiyE4jc9SOTyRa/Felnoo?type=design&node-id=0%3A1&mode=dev',
         github: 'https://github.com/Mouin-bkr'
     },
@@ -59,7 +59,7 @@ const data = [
         category: 'backend',
         image: Image6,
         title: "Movie Hub ",
-        desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+        desc: " A dynamic web application leveraging APIs to fetch movies along with their ratings, serving as a convenient platform for users seeking recommendations on what movie to watch. It provides a user-friendly interface for browsing and exploring a wide range of films.",
         demo: 'https://moviehuub.netlify.app',
         github: 'https://github.com/Mouin-bkr/movie-hub'
     },
@@ -78,7 +78,7 @@ const data = [
         category: 'backend',
         image: Image8,
         title: "Connect 4 ",
-        desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+        desc: "An engaging web-based rendition of the classic Connect 4 game, offering players the opportunity to compete against either another player or an AI opponent seamlessly integrated within the game.",
         demo: 'https://conect4game.netlify.app',
         github: 'https://github.com/Mouin-bkr/Connect4'
     },
@@ -87,7 +87,7 @@ const data = [
         category: 'frontend',
         image: Image9,
         title: "Birthday Wish ",
-        desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
+        desc: "A lighthearted website designed as a playful joke for a friend or loved one's birthday celebration. Featuring an interactive 'Click Me' button that playfully changes positions with each click, culminating in a delightful display of fireworks to mark the special occasion.",
         demo: 'https://mouin-bkr.github.io/Birthday-Wish/',
         github: 'https://github.com/Mouin-bkr/Birthday-Wish'
     }
